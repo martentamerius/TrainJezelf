cbdbbed847e6b121f60c649e55bb2f33945c52a4
Ronald Bos <ronald.bos.msc@gmail.com>
2014-08-12 23:56:44 +0200

    improve list view layout

--- android/app/src/main/java/com/example/ronald/trainjezelf/ReminderListFragment.java
+++ android/app/src/main/java/com/example/ronald/trainjezelf/ReminderListFragment.java
@@ -1,7 +1,7 @@
 package com.example.ronald.trainjezelf;
 
-import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
